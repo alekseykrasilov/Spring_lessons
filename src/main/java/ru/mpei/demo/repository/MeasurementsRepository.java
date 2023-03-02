9b043cee3ccd806b8cf9ef141ef46e454dba1a00
Aleksey Krasilov <alekseykrasilov_2015@mail.ru>
2023-03-02 22:31:45 +0300

    Second commit

--- src/main/java/ru/mpei/demo/repository/MeasurementsRepository.java
+++ src/main/java/ru/mpei/demo/repository/MeasurementsRepository.java
@@ -1,13 +1,30 @@
 package ru.mpei.demo.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.mpei.demo.model.Measurement;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
+@Transactional
 public class MeasurementsRepository {
 
     @PersistenceContext
     private EntityManager em;
+
+    public boolean save(Measurement m) {
+        if (m.getId() == 0) {
+            em.persist(m);
+        } else {
+            em.merge(m);
+        }
+        return true;
+    }
+
+    public Measurement getById(long id) {
+        Measurement measurement = em.find(Measurement.class, id);
+        return measurement;
+    }
 }
